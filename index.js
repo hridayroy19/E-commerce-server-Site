b739e08dad3778dbadd0b63cf4c9110a9736fb84
hridayroy19 <hrhridoyroy503@gmail.com>
2023-12-31 15:40:20 +0600

    post product mongodb

--- index.js
+++ index.js
@@ -11,12 +11,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-console.log( process.env.BD_pass);
 
-
-
-
-const uri = `mongodb+srv://${process.env.BD_USER}:${process.env.BD_PASS}@cluster0.jg43ilw.mongodb.net/?retryWrites=true&w=majority1`;
+const uri = `mongodb+srv://${process.env.BD_USER}:${process.env.BD_PASS}@cluster0.jg43ilw.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -32,11 +28,28 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+     const productCollation = client.db("vivabuy").collection("products");
+
+
+    //  app.get('/product/:id', async(req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: new ObjectId(id)};
+    //   const result = await productCollation.findOne(query);
+    //   res.send(result);
+    // })
+
+     app.post("/products", async (req , res)=>{
+      const newProduct = req.body;
+      console.log(newProduct);
+       const result= await productCollation.insertOne(newProduct);
+       res.send(result)
+     })
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
